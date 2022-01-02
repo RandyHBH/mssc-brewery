6371bb14fe64d35f9f72441a29adeda169fe6336
Randy Hector Bartumeu Huergo <randyhbh91@gmail.com>
2022-01-02 17:14:34 +0100

    Add customer controller

---
+++ src/main/java/com/bartumeu/msscbrewery/web/controller/CustomerController.java
@@ -0,0 +1,27 @@
+package com.bartumeu.msscbrewery.web.controller;
+
+import com.bartumeu.msscbrewery.web.model.CustomerDTO;
+import com.bartumeu.msscbrewery.web.service.CustomerService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/v1/customer")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("{customerId}")
+    public ResponseEntity<CustomerDTO> getCustomer(@PathVariable UUID customerId) {
+        return ResponseEntity.ok(customerService.getCustomerById(customerId));
+    }
+}
